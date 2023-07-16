ef1b6b24e592757468378e83d37ad8745a650187
Huzaifa Ayub <69161439+HuzaifaAyub@users.noreply.github.com>
2023-07-16 05:02:18 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,133 +1,112 @@
-let areaOfRectangle = (length,width) =>{
+function addNumbers (num1,num2){
+    return num1 + num2;
+}
+var result = addNumbers(2,58);
+console.log(result);
+
+    
+for (
+    var i = 1; i <= 10; i++
+)
+{
+    console.log(i);
+}
+function factorial(number) {
+    if (number === 0 || number === 1) { 
+    return 1 ;
+}
+var result = 1;
+for ( var i = 2 ; i<= number; i++) {
+    result *= i;
+}
+return result;
+}
+
+//Example
+var inputNmuber = 5;
+var result = factorial(inputNmuber);
+console.log(result);
+//Even numbers
+var number = 1;
+
+while (number <= 20) {
+    if (number % 2===0) {
+        console.log(number);
+    }
+    number++
+}
+  function calculateSum(numbers){
+    var sum = 0;
+    for (var i = 0 ; i < number.lenght; i++){
+        sum += numbers[i];
+    }
+    return sum;
+  }
+  // Example
+  var numbers = [1,2,3,4,5];
+  var result = calculateSum(numbers);
+  console.log(result);
+  function fibonacciSequence(number)
+  {
+    var sequence = [0,1];
+    for (var i = 2; i <= number; i++) {
+        var nextNumber = sequence[i - 1] + sequence[i - 2];
+        if (nextNumber <= number){
+            sequence.push(nextNumber);
+        } else {
+            break;
+        }
+
+    }
+    return sequence
+  }
+
+  function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt (number); i++){
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+  }
+   
+   let areaOfRectangle = (length,width) =>{
        
-    let area = (length*width);
+     let area = (length*width);
+     return area;
+   }
+
+   console.log(areaOfRectangle(4,7));
+  
+  
+    
+   let areaOfTriangle = (base,height) =>{
+       
+    let area = (base*height)/2;
     return area;
   }
 
-  console.log(areaOfRectangle(4,7));
- 
- 
-   
-  let areaOfTriangle = (base,height) =>{
-      
-   let area = (base*height)/2;
-   return area;
- }
+  console.log(areaOfTriangle(40,54));
 
- console.log(areaOfTriangle(40,54));
-
- let areaOfCircle = ( radius  ) =>{
-      
-   let area = 3.142*(radius*radius)
-   return area;
- }
-
- console.log(areaOfCircle(80));
-
-let findNumberSign = (num) => {
-    if (num>0){
-        return "positive";
-    }
-    else if (num<0){
-        return "negative";
-    } 
-    
-    {
-        return "zero";
-    }
-}
-console.log (findNumberSign(78));
-console.log(findNumberSign(0))
-console.log(findNumberSign(-323));
-
-
-
-const findLeapYear = (year) => {
-    if (year % 4==0) {
-        return "leap year"
-    }
-    else if (year % 4!=0){
-        return "not a leap year"
-    }
-    
-}
-
-console.log(findLeapYear(2024));
-
-
-
-
-
-let ageOfUsers = (age) =>{
-    if(age<15){
-        return "They are not eligible to vote"
-    }
-    else if(age<=18 || age<=65 ){
-        return "They are  eligible to vote"
-    }
-    else { return "They are  eligible for senior citizens benefits"
-}
+  let areaOfCircle = ( radius  ) =>{
        
-}
-console.log(ageOfUsers(43));
-console.log(ageOfUsers(10));
-console.log(ageOfUsers(90));
+    let area = 3.142*(radius*radius)
+    return area;
+  }
+
+  console.log(areaOfCircle(80));
 
 
+  let areaOfCircle2 = ( radius  ) =>{
+       
+    let area = 3.142*(Ma)
+    return area;
+  }
 
-let findWeekdays =(weekNumber) =>{
-    let week= ("sundays" ,"Monday" ,'tuesday' ,'wed' ,'thurs' , 'fri' ,'sat');
-    if (weekNumber >=1 && weekNumber <=7) {
-        return week=(weekNumber-1);
-    }
-    else{
-         return 'invalid number number must greater than equals to one and less than equals to seven'; 
-    }
-}
-console.log(findWeekdays(7));
-
-let getDiscount = (totalAmount) => {
-   switch(true){
-    case (totalAmount >= 3000):{ console.log ("discount is 10%");
-    break;}
-   
-    case  (totalAmount >= 5000):{ console.log ("discount is 5%");
-    break;}
-   
-    case  (totalAmount >= 10000):{ console.log ("discount is 3%");
-    break;}
-    default:{
-        console.log("pardon there will be no discount");
-    }
-
-    }
-   }
-console.log(getDiscount(50875));
-console.log(getDiscount(5000));
-
-const sumNumber = (num) =>{
-    let i;
-    let sum=0;
-    for (i=0 ; i< num; i++){
-        sum +=i;
-        console.log(sum);
-    }
-    console.log(sum);
-}
-// sumNumber(30);
-sumNumber(90);
+  console.log(areaOfCircle(80));
 
 
-const generateTables = (num)  => {
-    for (let i= 1; i <= 10; i++){
-     console.log (num  + "x" + i + "=" +(i*num));
-    }
-}
-generateTables(20);
-
-
-const FindWord= (word) =>  {
-    
-    
-}
-
+  
